9477755cebacdf79ba281f15a58b88601c46166e
Kieran Lucas <kieranlucas.work@gmail.com>
2025-09-21 09:56:10 +0700

    Add up-down movement for Dragon

---
+++ Assets/Dragon.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{  
+    public Rigidbody2D myRigidbody2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.linearVelocity = Vector2.up * 10;
+        }
+        
+    }
+}
